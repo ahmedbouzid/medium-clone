776685cfbbd9d5139215fd77a391d76671cc3836
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-09-30 10:41:51 +0200

    -prepare form logic - install ngrx

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistreComponent } from './registre/registre.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 const routes = [
@@ -13,6 +14,8 @@ const routes = [
   ],
   imports: [
     CommonModule ,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ]
 })
--- src/app/auth/registre/registre.component.ts
+++ src/app/auth/registre/registre.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registre',
   templateUrl: './registre.component.html',
   styleUrls: ['./registre.component.scss']
 })
-export class RegistreComponent {
+export class RegistreComponent implements OnInit {
 
+
+  registForm !: FormGroup ;
+
+  constructor(private fb : FormBuilder){}
+
+  ngOnInit(): void {
+this.initializeForm()
+
+  }
+
+  initializeForm(){
+    this.registForm = this.fb.group({
+      username : ['' , [Validators.required]] ,
+      email : ['' , [Validators.required]] ,
+      password : ['' , [Validators.required]]
+    })
+  }
+
+  submitForm(){
+    if(this.registForm.valid)
+    console.log(this.registForm.value);
+
+  }
 }
